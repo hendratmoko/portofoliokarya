8536643d0c972e0834414eca5ef9493775ca58f3
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-07-07 10:03:49 +0700

    Update config6.js

--- config6.js
+++ config6.js
@@ -2,22 +2,18 @@
 CONFIGURATION
 ==========================*/
 const CONFIG={
-
 /*==========================
 GOOGLE APPS SCRIPT
 ==========================*/
 WEBAPP_URL:"https://script.google.com/macros/s/AKfycbxMgVuBdoiD13vNlLrhUmIe-WtU9jCHU1BXuuv1wwDR3tSzo20pE31rJAjVoHr5g3V3Lg/exec",
-
 /*==========================
 GOOGLE SHEET
 ==========================*/
 SHEET_NAME:"GaleriKarya",
-
 /*==========================
 LOGIN ADMIN
 ==========================*/
 ACCESS_CODE:"AAA",
-
 /*==========================
 GALERI
 ==========================*/
@@ -25,7 +21,6 @@ ITEM_PER_LOAD:9,
 DEFAULT_SORT:"terbaru",
 DEFAULT_RATING:5,
 DEFAULT_VIEW:0,
-
 /*==========================
 SEKOLAH
 ==========================*/
@@ -33,25 +28,21 @@ SCHOOL_NAME:"SMK Negeri 1 Sanden",
 SCHOOL_SHORT:"SMKN1 Sanden",
 APP_NAME:"Galeri Karya Digital",
 APP_VERSION:"1.0.0",
-
 /*==========================
 SERTIFIKAT
 ==========================*/
 ENABLE_CERTIFICATE:true,
 CERTIFICATE_TEMPLATE:"default",
-
 /*==========================
 WHATSAPP
 ==========================*/
 ENABLE_WHATSAPP:true,
 COUNTRY_CODE:"62",
-
 /*==========================
 ADMIN
 ==========================*/
 SESSION_NAME:"adminLogin",
 SESSION_TIMEOUT:86400000,
-
 /*==========================
 MEDIA
 ==========================*/
@@ -64,7 +55,6 @@ ALLOW_PPT:true,
 ALLOW_EXCEL:true,
 ALLOW_DRIVE:true,
 ALLOW_YOUTUBE:true,
-
 /*==========================
 THUMBNAIL
 ==========================*/
@@ -76,77 +66,51 @@ EXCEL_IMAGE:"assets/excel.png",
 VIDEO_IMAGE:"assets/video.png",
 AUDIO_IMAGE:"assets/audio.png",
 DRIVE_IMAGE:"assets/drive.png",
-
 /*==========================
 ANIMATION
 ==========================*/
 LOADER_DELAY:800,
 SEARCH_DELAY:300,
 TOAST_DELAY:2500,
-
 /*==========================
 THEME
 ==========================*/
 DEFAULT_THEME:"light",
-
 /*==========================
 DEBUG
 ==========================*/
 DEBUG:true
-
 };
-
 /*==========================
 FREEZE CONFIG
 ==========================*/
 Object.freeze(CONFIG);
-
 /*==========================
 HELPER
 ==========================*/
 function getConfig(key){
-
 return CONFIG[key];
-
 }
-
 function isDebug(){
-
 return CONFIG.DEBUG===true;
-
 }
-
 function appVersion(){
-
 return CONFIG.APP_VERSION;
-
 }
-
 function appName(){
-
 return CONFIG.APP_NAME;
-
 }
-
 function schoolName(){
-
 return CONFIG.SCHOOL_NAME;
-
 }
-
 /*==========================
 CONSOLE
 ==========================*/
 if(CONFIG.DEBUG){
-
 console.log("================================");
-
 console.log(CONFIG.APP_NAME);
-
 console.log("Version :",CONFIG.APP_VERSION);
-
 console.log("School :",CONFIG.SCHOOL_NAME);
-
 console.log("Debug :",CONFIG.DEBUG);
 
 console.log("================================");
